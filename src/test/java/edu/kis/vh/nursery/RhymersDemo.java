c02227d021df7bf84bbfca58c74ac57fd379abca
Artur <pedrolopezv18@gmail.com>
2019-12-03 14:43:49 +0100

    stale

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -5,19 +5,23 @@ import edu.kis.vh.nursery.factory.Rhymersfactory;
 
 class RhymersDemo {
 
+    public static final int SIZE1 = 15;
+    public static final int SIZE2 = 3;
+    public static final int BOUND = 20;
+
     public static void main(String[] args) {
         Rhymersfactory factory = new DefaultRhymersFactory();
 
         DefaultCountingOutRhymer[] rhymers = {factory.GetStandardRhymer(), factory.GetFalseRhymer(),
                 factory.GetFIFORhymer(), factory.GetHanoiRhymer()};
 
-        for (int i = 1; i < 15; i++)
-            for (int j = 0; j < 3; j++)
+        for (int i = 1; i < SIZE1; i++)
+            for (int j = 0; j < SIZE2; j++)
                 rhymers[j].countIn(i);
 
         java.util.Random rn = new java.util.Random();
-        for (int i = 1; i < 15; i++)
-            rhymers[3].countIn(rn.nextInt(20));
+        for (int i = 1; i < SIZE1; i++)
+            rhymers[3].countIn(rn.nextInt(BOUND));
 
         for (int i = 0; i < rhymers.length; i++) {
             while (!rhymers[i].callCheck())
@@ -26,7 +30,7 @@ class RhymersDemo {
         }
 
         System.out.println("total rejected is "
-                + ((HanoiRhymer) rhymers[3]).reportRejected());
+                + ((HanoiRhymer) rhymers[SIZE2]).reportRejected());
 
     }
 
